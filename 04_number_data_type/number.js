5ecb9c91a7f43bec5cea26b89625590248f2c5e3
Tutul Kabir <tutulkabir@gmail.com>
2025-04-15 10:16:00 +0530

    number data type

---
+++ 04_number_data_type/number.js
@@ -0,0 +1,14 @@
+// var var_name : datatype = value;
+var num1 = 20;
+var num2 = 30;
+var total = num1 + num2;
+console.log(total);
+var oct = 262145;
+var hex = 0x000001;
+var bin = 1;
+console.log(oct);
+//conversion of string to number
+var item1 = 20;
+var item2 = "30";
+var total = item1 + Number(item2);
+console.log(total);
---
+++ 04_number_data_type/number.ts
@@ -0,0 +1,17 @@
+// var var_name : datatype = value;
+
+var num1: number = 20;
+var num2: number = 30;
+var total: number = num1 + num2;
+console.log(total);
+
+var oct: number = 0o1000001;
+var hex: number = 0x000001;
+var bin: number = 0b0000001;
+console.log(oct);
+
+//conversion of string to number
+var item1: number = 20;
+var item2 = "30";
+var total = item1 + Number(item2);
+console.log(total);
